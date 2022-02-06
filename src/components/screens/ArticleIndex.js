b30a9e91e0239a2857e625e53e859b45b4e09033
RaquelV <raquelv85@gmail.com>
2022-02-06 21:50:53 +0100

    install material ui and add function to search articles

--- src/components/screens/ArticleIndex.js
+++ src/components/screens/ArticleIndex.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+
+import InputBase from "@mui/material/InputBase";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 //components
 import { Article } from "../Article";
@@ -6,14 +11,49 @@ import { Article } from "../Article";
 export const ArticleIndex = (props) => {
   const { resultsFeed } = props;
 
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchArticle = () => {
+    let filterArticle = resultsFeed.articles;
+
+    if (searchValue !== "") {
+      filterArticle = resultsFeed.articles.filter((elem) =>
+        elem.title.toUpperCase().includes(searchValue.toUpperCase())
+      );
+    }
+   
+    return filterArticle;
+  };
+
   return (
     <div className="App">
+      <form>
+        <Paper
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search article"
+            inputProps={{ "aria-label": "search article" }}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </form>
+
       {Object.keys(resultsFeed).length > 0 &&
-        resultsFeed.articles.map((article, indexArt) => {
+        searchArticle().map((article, indexArt) => {
           return <Article key={indexArt} article={article} />;
         })}
     </div>
   );
-}
+};
 
 export default ArticleIndex;
